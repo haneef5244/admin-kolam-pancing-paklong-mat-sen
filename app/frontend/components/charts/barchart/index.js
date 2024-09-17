96831f8ad54cb8724aedb04ab728b906b38729b3
Haneef <haneef5244@gmail.com>
2024-09-18 00:10:16 +0800

    fix date off by 1 day bug highchart

--- app/frontend/components/charts/barchart/index.js
+++ app/frontend/components/charts/barchart/index.js
@@ -1,7 +1,11 @@
 import HighchartsReact from 'highcharts-react-official';
 import React from 'react';
 import Highcharts from "highcharts";
-
+Highcharts.setOptions({
+    global: {
+        useUTC: false
+    }
+});
 
 export const BarChart = (props) => {
     const chartOptions = {
